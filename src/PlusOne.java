979be1cc825f583e7381fa12ba2668996bad5415
Jon Biolette <jonbiolette@gmail.com>
2022-03-20 00:32:07 -0700

    Removed test cases

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -6,13 +6,7 @@
 import java.math.BigInteger;
 import java.util.Arrays;
 
-public class PlusOne {
-    public static void main(String[] args) {
-        int[] nums = new int[]{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
-        System.out.println(Arrays.toString(plusOne(nums)));
-    }
-
-    public static int[] plusOne(int[] digits) {
+public static int[] plusOne(int[] digits) {
         String numS = "";
 
         //Convert to string to combine all index's
